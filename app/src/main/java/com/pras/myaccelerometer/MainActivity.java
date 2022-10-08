ffcdc732d8729da6dd79dc0acb84f5414295aa0a
Prasetya Adi Arfanto <84487845+prasetya4di@users.noreply.github.com>
2022-10-08 09:39:05 +0700

    Import view to activity_main.xml

--- app/src/main/java/com/pras/myaccelerometer/MainActivity.java
+++ app/src/main/java/com/pras/myaccelerometer/MainActivity.java
@@ -1,14 +1,44 @@
 package com.pras.myaccelerometer;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private TextView txtAxisX, txtAxisY, txtAxisZ;
+    private ImageView imgIllustration;
+    private ListView lvNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtAxisX = findViewById(R.id.txtAxisX);
+        txtAxisY = findViewById(R.id.txtAxisY);
+        txtAxisZ = findViewById(R.id.txtAxisZ);
+        imgIllustration = findViewById(R.id.imgIllustration);
+        lvNavigation = findViewById(R.id.listNavigation);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
