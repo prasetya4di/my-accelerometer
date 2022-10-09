20cfb667854e54456fa952a0ecafa48be8ba1f02
Prasetya Adi Arfanto <84487845+prasetya4di@users.noreply.github.com>
2022-10-09 17:30:47 +0700

    Add pointing parser

--- app/src/main/java/com/pras/myaccelerometer/Pointing.java
+++ app/src/main/java/com/pras/myaccelerometer/Pointing.java
@@ -4,5 +4,24 @@ enum Pointing {
     UP,
     DOWN,
     LEFT,
-    RIGHT
+    RIGHT;
+
+    static Pointing parse(float x, float y) {
+        // Pointing up or down
+        if (x > -4 && x < 4) {
+            if (y > 0) {
+                return Pointing.UP;
+            } else {
+                return Pointing.DOWN;
+            }
+        }
+
+        // Pointing left or right
+        if (x < 0) {
+            return Pointing.RIGHT;
+        } else if (x > 0) {
+            return Pointing.LEFT;
+        }
+        return null;
+    }
 }
